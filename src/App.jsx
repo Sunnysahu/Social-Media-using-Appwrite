5643b9fae0d0cc7c436833f4f47ab6bb589b765a
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-08 00:00:44 +0530

    Added ENV Variables and exported them

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  // console.log(process.env.REACT_APP_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
   return (
     <>
       <div className="bg-red-100">Hello Sunny!!!</div>
