213e7b5aafbe9cb2980bc3e05120412caf076ca9
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-10 00:39:12 +0530

    Implemented Appwrite Functionality

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -3,9 +3,61 @@ import conf from "../conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
-  constructor() {}
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setEndpoint(conf.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        password,
+        name
+      );
+      if (userAccount) {
+        //Call another method
+        return this.login(email, password);
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser", error);
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: Delete Sessions", error);
+    }
+  }
 }
 
-const AuthService = new AuthService();
+const authService = new AuthService();
 
 export default authService;
