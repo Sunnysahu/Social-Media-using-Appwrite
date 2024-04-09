6a68086f41a09259fd84df7b97b10cf139c861d6
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-10 00:56:53 +0530

    Implementing Updating Functions

---
+++ src/appwrite/config.js
@@ -0,0 +1,43 @@
+import conf from "../conf";
+
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setEndpoint(conf.appwriteProjectId);
+
+    this.databases = new Databases(this.client);
+    this.storage = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: createPost :: error", error);
+    }
+  }
+
+  async updatePost(slug, { title, content, featuredImage, status }) {}
+}
+
+const service = new Service();
+
+export default service;
