3e23ccb2a4de217af58faadbb6e2ccfc7a90814c
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-12 00:47:14 +0530

    Added File Functionility

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -84,15 +84,44 @@ export class Service {
     try {
       return await this.databases.listDocuments(
         conf.appwriteDatabaseId,
-        conf.appwriteCollectionId
+        conf.appwriteCollectionId,
+        queries,
+        100,
+        0
       );
     } catch (error) {
       console.log("Appwrite Service :: listDocuments :: error", error);
       return false;
     }
   }
-}
 
+  async uploadFile(file) {
+    try {
+      return await this.storage.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("Appwrite Service :: uploadFile :: error", error);
+      return false;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("Appwrite Service :: deleteFile :: error", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
+}
 const service = new Service();
 
 export default service;
