85715d1263b9ff01fa349ae5ffedd188b75267ae
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-12 00:17:47 +0530

    Added Database Functionality

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -35,7 +35,62 @@ export class Service {
     }
   }
 
-  async updatePost(slug, { title, content, featuredImage, status }) {}
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: updatePost :: error", error);
+    }
+  }
+
+  async deletePost(slug, { title, content, featuredImage, status }) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite Service :: deletePost :: error", error);
+      return false;
+    }
+  }
+
+  async getPost({ slug }) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("Appwrite Service :: getDocument :: error", error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId
+      );
+    } catch (error) {
+      console.log("Appwrite Service :: listDocuments :: error", error);
+      return false;
+    }
+  }
 }
 
 const service = new Service();
