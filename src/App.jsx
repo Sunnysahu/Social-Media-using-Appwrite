7d0c9a5c1f89f464d91fa22fec4f82694704fc1c
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-13 00:22:22 +0530

    Encoded Store and Other Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+
+import authService from "./appwrite/auth";
+
+import { login, logout } from "./store/authSlice";
+
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import { Outlet } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
-  // console.log(process.env.REACT_APP_APPWRITE_URL);
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
 
-  return (
-    <>
-      <div className="text-white">Hello Sunny!!!</div>
-    </>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  // const userloggedIn
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
