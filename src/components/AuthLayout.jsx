7a05635f02faefb198aca6a284913c927055c630
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-16 15:45:59 +0530

    Done

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,31 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+export default function Protected({children, authentication = true}) {
 
-export default function Protected({ children, authentication = true }) {
-  const navigate = useNavigate();
-  const [loader, setLoader] = useState(true);
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
+    const authStatus = useSelector(state => state.auth.status)
 
-  const authStatus = useSelector((state) => state.auth.status);
+    useEffect(() => {
+        //TODO: make it more easy to understand
 
-  useEffect(() => {
-    //TODO: Make it more Easy to Understand
+        // if (authStatus ===true){
+        //     navigate("/")
+        // } else if (authStatus === false) {
+        //     navigate("/login")
+        // }
+        
+        //let authValue = authStatus === true ? true : false
 
-    // if (authStatus == true) {
-    //   navigate("/");
-    // } else if (authStatus == false) {
-    //   navigate("/login");
-    // }
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, navigate, authentication])
 
-    // let authValue = authStatus == true ? true : false;
-
-    if (authentication && authStatus !== authentication) {
-      navigate("/login");
-    } else if (!authentication && authStatus !== authentication) {
-    }
-    setLoader(false);
-  }, [authStatus, navigate, authentication]);
-
-  return loader ? <h1>Loading...</h1> : <>{children}</>;
+  return loader ? <h1>Loading...</h1> : <>{children}</>
 }
+
